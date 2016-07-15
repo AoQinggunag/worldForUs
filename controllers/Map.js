32dce95d7e741373b65cdc23bb3480d141e50a75
0604hx <1053214511@qq.com>
2016-07-15 15:35:21 +0800

    显示旅行计划

--- controllers/Map.js
+++ controllers/Map.js
@@ -1,9 +1,13 @@
 /**
  * Created by zengxm on 2016/7/13.
  */
+
+var fs=require("fs");
+
 exports.install=function(){
     F.route("/map",index);
     F.route("/trips",trips)
+    F.route("/tripsFile", readTripsFile);
 };
 
 /**
@@ -11,7 +15,7 @@ exports.install=function(){
  */
 function index(){
     var self = this;
-    self.view('index');
+    self.view('index',{transports:CONFIG("transports")});
 }
 
 /**
@@ -37,10 +41,35 @@ function trips(){
                     lng:113.380971
                 }
             ],
-            startAt:new Date(2015,1,18),
-            endAt:new Date(2015,1,20),
+            startAt:"2015-01-18",
+            endAt:"2015-01-20",
             transport:1,
             remark:"长隆欢乐世界",
+            category:"情侣出行"
+        },
+        {
+            id:"1",
+            cities:[
+                {
+                    name:"中国 广西 南宁",
+                    lat:22.854974,
+                    lng:108.284869
+                },
+                {
+                    name:"中国 海南 海口",
+                    lat:20.043582,
+                    lng:110.344116
+                },
+                {
+                    name:"中国 海南 三亚",
+                    lat: 18.25209,
+                    lng:109.516
+                }
+            ],
+            startAt:"2015-06-18",
+            endAt:"2015-06-20",
+            transport:3,
+            remark:"",
             category:"情侣出行"
         },
         {
@@ -57,11 +86,24 @@ function trips(){
                     lng:112.893119
                 },
             ],
-            startAt:new Date(2016,1,1),
-            endAt:new Date(2016,1,3),
+            startAt:"2016-01-01",
+            endAt:"2015-01-03",
             transport:1,
             remark:"元旦出行",
             category:"情侣出行"
         }
     ]);
+}
+
+function readTripsFile(){
+    var self=this;
+    //读取文件
+    var filePath=F.path.private("trips.json");
+    var stream=fs.createReadStream(filePath, {flags:'r',encoding:"utf-8"});
+
+    stream.on("error",function(){
+        self.send(404);
+    });
+
+    stream.pipe(self.res);
 }
